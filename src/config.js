11582952d3c77db897a7a4b3521bf8c90b4e979b
Niemietz <renan_ncs@msn.com>
2026-08-24 01:02:55 +0100

    Update index page to deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_SERVER_URL = 'localhost:3000'; // 'android-app-generator-7c4c2b5118c7.herokuapp.com'
-const USE_HTTPS = false; // true
+const API_SERVER_URL = 'android-app-generator-7c4c2b5118c7.herokuapp.com'
+const USE_HTTPS = true
 
 export const API_BASE_URL = `${USE_HTTPS ? 'https' : 'http'}://${API_SERVER_URL}`;
